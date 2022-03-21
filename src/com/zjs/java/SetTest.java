4925a9c61470dbb3f2f43275b735a3ae2ae25b91
Zjshadow <2573581779@qq.com>
2022-03-21 22:28:56 +0800

    Merge remote-tracking branch 'origin/master'

--- src/com/zjs/java/SetTest.java
+++ src/com/zjs/java/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -30,6 +31,30 @@ public class SetTest {
         set.add(456);
         set.add("fgd");
         set.add("book");
+        set.add(new User("Tom",12));
+        set.add(new User("Tom",12));
+        set.add(129);
+
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    /**
+     * LinkedHashSet的使用
+     * LinkedHashSet作为HashSet的子类，在添加数据的同时，每个数据还维护了两个引用，记录此数据前一个
+     * 数据和后一个数据。
+     * 优点：对于频繁的遍历操作，LinkedHashSet效率高于HashSet
+     */
+    @Test
+    public void test2(){
+        Set set = new LinkedHashSet();
+        set.add(456);
+        set.add(123);
+        set.add(123);
+        set.add("AA");
+        set.add("CC");
         set.add(new User("Tom",12));
         set.add(new User("Tom",12));
         set.add(129);
