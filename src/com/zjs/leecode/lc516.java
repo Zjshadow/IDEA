17a1f22be077767ad5bd66f9a3a3bb1e1be5b8b8
Zjshadow <2573581779@qq.com>
2022-05-21 12:18:57 +0800

    update

--- src/com/zjs/leecode/lc516.java
+++ src/com/zjs/leecode/lc516.java
@@ -1,4 +1,43 @@
 package com.zjs.leecode;
 
+import org.junit.Test;
+
 public class lc516 {
+    @Test
+    public void test(){
+        String s = "aaaba";
+        int res = longestPalindromeSubseq(s);
+        System.out.println(res);
+    }
+
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 0)
+                        dp[i][j] = 1;
+                    else if (j - i == 1)
+                        dp[i][j] = 2;
+                    else
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[0][n - 1];
+    }
 }
--- src/com/zjs/leecode/lc583.java
+++ src/com/zjs/leecode/lc583.java
@@ -1,4 +1,45 @@
 package com.zjs.leecode;
 
+import org.junit.Test;
+
 public class lc583 {
+
+    @Test
+    public void test(){
+        String w1 = "sea";
+        String w2 = "eat";
+        int minDis = minDistance(w1, w2);
+        System.out.println(minDis);
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        int[][] dp = new int[n1 + 1][n2 + 1];
+
+        for (int i = 1; i <= n1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= n2; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]) + 1, dp[i - 1][j - 1] + 2);
+            }
+        }
+
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return dp[n1][n2];
+    }
 }
