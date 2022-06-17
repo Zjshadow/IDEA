0c011f68280b36de7ce097c28d0d152c75dc6023
Zjshadow <2573581779@qq.com>
2022-06-17 11:12:14 +0800

    update

--- src/com/zjs/leecode/lc452.java
+++ src/com/zjs/leecode/lc452.java
@@ -1,9 +1,52 @@
 package com.zjs.leecode;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author zjs
  * @version 1.0
  * @data 2022/6/7 17:10
  **/
 public class lc452 {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test() {
+        int[][] points = new int[][] {{-2147483646,-2147483645},{2147483646,2147483647}};
+//        int[][] points = new int[][] {{1,5},{3,4}};
+        int res = findMinArrowShots(points);
+        System.out.println(res);
+    }
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                if (a[1] > b[1]) {
+                    return 1;
+                } else if (a[1] < b[1]) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+                // return a[1] - b[1];
+            }
+        });
+
+        // int cnt = 0;
+        // int end = Integer.MIN_VALUE;
+        // 错解：[[-2147483648,2147483647]]
+        int cnt = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (end < points[i][0]) {
+                end = points[i][1];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
--- src/com/zjs/leecode/lc56.java
+++ src/com/zjs/leecode/lc56.java
@@ -1,9 +1,61 @@
 package com.zjs.leecode;
 
+import org.junit.Test;
+
+import javax.sound.midi.Soundbank;
+import java.util.*;
+
 /**
  * @author zjs
  * @version 1.0
  * @data 2022/6/10 21:36
  **/
 public class lc56 {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test(){
+        int[][] intervals = new int[][] {{1,3},{2,6},{8,10},{15,18}};
+        int[][] ans = merge(intervals);
+        System.out.println();
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] merge(int[][] intervals) {
+        int cnt = 0;
+        int n = intervals.length;
+        // 按照右边边界排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[1] - interval2[1];
+            }
+        });
+        // 从左向右，优先选择右边界小的
+        List<int[]> res = new ArrayList<int[]>();
+        int start = intervals[0][0], end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            // 注意是等于 区间边界可以重叠[0,1][1,2]
+            if (end <= intervals[i][0]) {
+                end = intervals[i][1];
+            }
+            else {
+                start = intervals[i][0];
+                start = intervals[i][1];
+            }
+            res.add(new int[]{start, end});
+        }
+        // cnt为不重叠数量，n-cnt即为需要删除的重叠数量
+        int[][] ans = new int[res.size()][];
+        return res.toArray(ans);
+
+    }
 }
