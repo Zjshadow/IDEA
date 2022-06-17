0f88b3f6969edbe01f75b94412ad188899a018e7
Zjshadow <2573581779@qq.com>
2022-06-17 11:11:34 +0800

    update

---
+++ src/com/zjs/leecode/lc452.java
@@ -0,0 +1,9 @@
+package com.zjs.leecode;
+
+/**
+ * @author zjs
+ * @version 1.0
+ * @data 2022/6/7 17:10
+ **/
+public class lc452 {
+}
---
+++ src/com/zjs/leecode/lc56.java
@@ -0,0 +1,9 @@
+package com.zjs.leecode;
+
+/**
+ * @author zjs
+ * @version 1.0
+ * @data 2022/6/10 21:36
+ **/
+public class lc56 {
+}
