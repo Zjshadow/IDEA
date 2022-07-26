3e342d71719e4f6820f07010db39757e7897f5d1
Zjshadow <2573581779@qq.com>
2022-07-26 22:47:34 +0800

    update

--- src/com/zjs/Exam/zy_7_26_T1.java
+++ src/com/zjs/Exam/zy_7_26_T1.java
@@ -10,11 +10,35 @@ import java.util.*;
 public class zy_7_26_T1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
+        List<String> genes = new ArrayList<>();
+        List<String> person = new ArrayList<>();
+
+        int geneLen = in.nextInt();
         in.nextLine();
-        for (int i = 0; i < num; i++) {
-            String str = in.nextLine();
-            System.out.println(str);
+        while (geneLen-- > 0) {
+            genes.add(in.nextLine());
         }
+        int personLen = in.nextInt();
+        in.nextLine();
+        while (personLen-- > 0) {
+            person.add(in.nextLine());
+        }
+
+        int isGene;
+        for (int i = 0; i < person.size(); i++) {
+            isGene = 0;
+            for (int j = 0; j < genes.size(); j++) {
+                if (person.get(i).contains(genes.get(j))) {
+                    System.out.println("A new hero discovered");
+                    isGene = 1;
+                    break;
+                }
+            }
+
+            if (isGene == 0) {
+                System.out.println("Just an ordinary person");
+            }
+        }
+
     }
 }
--- src/com/zjs/Exam/zy_7_26_T2.java
+++ src/com/zjs/Exam/zy_7_26_T2.java
@@ -11,6 +11,36 @@ public class zy_7_26_T2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        sc.nextLine();
+
+        for (int i = 0; i < num; i++) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = sc.nextInt();
+            }
+            int[] b = new int[n];
+            for (int j = 0; j < n; j++) {
+                b[j] = sc.nextInt();
+            }
+
+            if (n == 1) {
+                System.out.println(a[0] > b[0] ? a[0] : b[0]);
+            }
+            else {
+                int[][] dp = new int[n + 1][2];
+                dp[1][0] = a[0];
+                dp[1][1] = b[0];
+                dp[2][0] = a[0] + a[1];
+                dp[2][1] = b[1];
+
+                for (int j = 2; j <= n; j++) {
+                    dp[j][0] = Math.max(dp[j - 1][0] + a[j - 1], dp[j - 2][1] + a[j - 1]);
+                    dp[j][1] = Math.max(dp[j - 1][1] + b[j - 1], dp[j - 2][0] + b[j - 1]);
+                }
+
+                System.out.println(dp[n][0] > dp[n][1] ? dp[n][0] : dp[n][1]);
+            }
+
+        }
     }
 }
