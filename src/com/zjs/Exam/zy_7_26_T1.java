7972be806000709814d500498b37006b9e6ba00e
Zjshadow <2573581779@qq.com>
2022-07-26 21:49:10 +0800

    update

--- src/com/zjs/Exam/zy_7_26_T1.java
+++ src/com/zjs/Exam/zy_7_26_T1.java
@@ -1,10 +1,20 @@
 package com.zjs.Exam;
 
+import java.util.*;
+
 /**
  * @author zjs
  * @version 1.0
- * @data 2022/7/26 21:14
+ * @data 2022/7/26 20:45
  **/
 public class zy_7_26_T1 {
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < num; i++) {
+            String str = in.nextLine();
+            System.out.println(str);
+        }
+    }
 }
