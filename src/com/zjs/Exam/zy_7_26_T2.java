8a1a7e02feefd41ff9b34dd1146bf6cb8ed0e094
Zjshadow <2573581779@qq.com>
2022-07-26 21:48:43 +0800

    update

---
+++ src/com/zjs/Exam/zy_7_26_T2.java
@@ -0,0 +1,16 @@
+package com.zjs.Exam;
+
+import java.util.*;
+
+/**
+ * @author zjs
+ * @version 1.0
+ * @data 2022/7/26 20:45
+ **/
+public class zy_7_26_T2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.nextLine();
+    }
+}
